72c30d4e326a1e5821a7e299b8de26f47aff18bf
Sergey Vetsel <wecelss@gmail.com>
2022-04-29 14:14:10 +0300

    Initial commit

---
+++ Flask_Auth/routes.py
@@ -0,0 +1,25 @@
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#Home route
+@app.route("/", methods=("GET", "POST"), strict_slashes=False)
+def index():
+    return render_template("index.html", title="Home")
+
+#Login route
+@app.route("/login/", methods=("GET", "POST"), strict_slashes=False)
+def login():
+    form = login_form()
+
+    return render_template("auth.html", form=form)
+
+#Register route
+@app.route("/register/", methods=("GET", "POST"), strict_slashes=False)
+def register():
+    form = register_form()
+
+    return render_template("auth.html", form=form)
+
+if __name__== "__main__"
+    app.run(debug=True)
